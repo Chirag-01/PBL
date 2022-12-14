c79b2a2966b6ad241b52e93999a9ce6c18e62f79
Chirag-01 <lalwanicp@rknec.edu>
2022-12-14 18:23:32 +0530

    Blink

---
+++ app/src/main/java/com/led/led/ledControl.java
@@ -0,0 +1,262 @@
+package com.led.led;
+
+import android.os.Handler;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.AsyncTask;
+
+import com.skydoves.colorpickerview.ColorListener;
+import com.skydoves.colorpickerview.ColorPickerView;
+
+import java.io.IOException;
+import java.util.UUID;
+
+
+public class ledControl extends AppCompatActivity {
+
+    Button btnOn, btnOff;
+    TextView rgbValueText;
+    ColorPickerView colorPickerView;
+    String address = null;
+    Boolean firstRun = true;
+    ProgressDialog progress;
+    Switch switch1;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    //SPP UUID. Look for it
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        Intent newint = getIntent();
+        address = newint.getStringExtra(DeviceList.EXTRA_ADDRESS); //receive the address of the bluetooth device
+
+        //view of the ledControl
+        setContentView(R.layout.activity_led_control);
+
+        //call the widgets
+        btnOn = findViewById(R.id.Upali_Button);
+        btnOff = findViewById(R.id.Ugasi_Button);
+
+        colorPickerView = findViewById((R.id.colorPickerView));
+        rgbValueText = findViewById((R.id.rgbValues));
+        switch1=findViewById(R.id.switch2);
+
+        new ConnectBT().execute(); //Call the class to connect
+        Log.d("switch",""+switch1);
+
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // on below line we are checking
+                // if switch is checked or not.
+                Log.d("switch",""+isChecked);
+                if (isChecked) {
+                    // on below line we are setting text
+                    // if switch is checked.
+
+                    LedBlink();
+                } else {
+                    // on below line we are setting text
+                    // if switch is unchecked.
+//                    statusTV.setText("Switch is UnChecked");
+                    turnOnLed();
+                }
+            }
+        });
+        //commands to be sent to bluetooth
+        btnOn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                turnOnLed();      //method to turn on
+            }
+        });
+
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                turnOffLed();   //method to turn off
+            }
+        });
+
+
+        colorPickerView.setColorListener(new ColorListener() {
+            @Override
+            public void onColorSelected(int color) {
+                int[] rgbValue = colorPickerView.getColorRGB();
+                final int redValue = rgbValue[0];
+                final int greenValue = rgbValue[1];
+                final int blueValue = rgbValue[2];
+                final String fullText = String.valueOf(String.format("%03d", redValue)) + String.valueOf(String.format("%03d", greenValue)) + String.valueOf(String.format("%03d", blueValue));
+
+                if (btSocket != null) {
+                    if (firstRun) {
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                firstRun = false;
+                                rgbValueText.setText(String.valueOf(fullText));
+                            }
+                        }, 1000);
+                    } else {
+                        try {
+                            btSocket.getOutputStream().write("RGB_".toString().concat(fullText).getBytes());
+
+                            rgbValueText.setText("RGB_".toString().concat(fullText));
+                        } catch (IOException e) {
+                            msg("Something went wrong when trying to change the color. Please try again or restart the app.");
+                        }
+                    }
+                }
+
+            }
+        });
+
+    }
+
+    private void turnOffLed()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write("LED_OFF".toString().getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+    private void LedBlink()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write("LED_BLINK".toString().getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+    private void turnOnLed()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write("LED_ON".toString().getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+    // fast way to call Toast
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_led_control, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute()
+        {
+            progress = ProgressDialog.show(ledControl.this, "Connecting...", "Please wait!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+            try
+            {
+                if (btSocket == null || !isBtConnected)
+                {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();//start connection
+                }
+            }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess)
+            {
+                msg("Connecting failed. Please try to pair the devices again.");
+                finish();
+            }
+            else
+            {
+                msg("Connected. Now you can change colors");
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
+    }
+}
